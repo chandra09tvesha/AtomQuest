fea5486d2c1eb776a104bc881b56968837c0af7d
chandra09tvesha <chandra09tvesha@gmail.com>
2026-05-19 02:24:33 +0530

    Update Goal.js

--- backend/models/Goal.js
+++ backend/models/Goal.js
@@ -1,13 +1,30 @@
 const mongoose = require("mongoose");
 
 const goalSchema = new mongoose.Schema({
-  employeeId: String,
-  title: String,
-  target: Number,
-  weightage: Number,
+  employeeId: { type: String, required: true },
+
+  title: { type: String, required: true },
+
+  target: { type: Number, required: true },
+
+  weightage: {
+    type: Number,
+    min: 10,
+    max: 100,
+    required: true
+  },
+
   achievement: { type: Number, default: 0 },
-  status: { type: String, default: "Not Started" },
-  approved: { type: Boolean, default: false }
+
+  status: {
+    type: String,
+    enum: ["Not Started", "On Track", "Completed"],
+    default: "Not Started"
+  },
+
+  approved: { type: Boolean, default: false },
+
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model("Goal", goalSchema);
